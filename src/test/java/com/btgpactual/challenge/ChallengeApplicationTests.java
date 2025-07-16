6ce7b68c5be0b2b9567d5003185f9cbf6e7d37a1
HenriqueMendesCoelho <henriquemendescoelho@gmail.com>
2025-07-15 23:09:20 -0300

    initial commit

---
+++ src/test/java/com/btgpactual/challenge/ChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.btgpactual.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
